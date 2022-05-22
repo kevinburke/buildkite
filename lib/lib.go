ed1d2c862d63f26d70a1e170abf13188b0fd9b09
Kevin Burke <kevin@burke.dev>
2022-05-21 20:36:02 -0700

    0.8

--- lib/lib.go
+++ lib/lib.go
@@ -18,7 +18,7 @@ import (
 )
 
 // The buildkite-go version. Run "make release" to bump this number.
-const Version = "0.7"
+const Version = "0.8"
 const userAgent = "buildkite-go/" + Version
 
 const APIVersion = "v2"
